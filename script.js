d4e374aa3f8fa6c461b250bee82800d4fa23e06e
CCaswell9 <calebcaswell9@gmail.com>
2024-10-01 14:33:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let slideIndex = 0;
+        const slides = document.getElementsByClassName("slide");
+        const totalSlides = slides.length;
+
+        function showSlides() {
+            for (let i = 0; i < totalSlides; i++) {
+                slides[i].style.display = "none";  
+            }
+            slideIndex++;
+            if (slideIndex > totalSlides) { slideIndex = 1 }    
+            slides[slideIndex - 1].style.display = "block";  
+            setTimeout(showSlides, 3000); // Change slide every 3 seconds
+        }
+
+        showSlides();
